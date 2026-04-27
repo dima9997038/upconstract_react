936e9cd10107135b8ee62a5c926cad6df84f369e
dima9997038 <dima9997038@gmail.com>
2026-04-27 09:10:09 +0300

    Deploy 15

--- src/pages/Sertificate.js
+++ src/pages/Sertificate.js
@@ -3,11 +3,12 @@ import FooterComponent from "../components/FooterComponent";
 import { Col, Container, Image, Row} from "react-bootstrap";
 import NavBarComponent from "../components/NavBarComponent";
 import isoImg from '../img/sertificate/iso.jpg'
-import isoEngImg from '../img/sertificate/isoEng.jpg'
-import attestat1 from '../img/sertificate/attestat1.jpg'
-import attestat2 from '../img/sertificate/attestat2.jpg'
-import attestat3 from '../img/sertificate/attestat3.jpg'
-import attestat4 from '../img/sertificate/attestat4.jpg'
+import isoEngImg from '../img/sertificate/Sert_page-0001.jpg'
+import attestat1 from '../img/sertificate/Sert_page-0002.jpg'
+import attestat2 from '../img/sertificate/Sert-0003.jpg'
+import attestat3 from '../img/sertificate/Sert_page-0004.jpg'
+import attestat4 from '../img/sertificate/Sert-0005.jpg'
+import attestat5 from '../img/sertificate/Sert-0006.jpg'
 
 function Sertificate(props) {
     return (
@@ -35,7 +36,9 @@ function Sertificate(props) {
                     <Col md="auto">
                         <Image src={attestat4} height="500"/>
                     </Col>
-
+                    <Col md="auto">
+                        <Image src={attestat5} height="500"/>
+                    </Col>
                 </Row>
             </Container>
             <FooterComponent/>
